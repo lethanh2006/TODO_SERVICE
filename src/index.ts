b8bced5f207a6d57e0d411a0a0fcac9d7526006e
lethanh2006 <thanhld2006.ptit@gmail.com>
2026-08-20 17:59:09 +0700

    Dọn tài liệu Swagger trùng lặp của Todo

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDb from './config/db.js';
 import taskRoutes from './routes/task.js';
-import swaggerJsdoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
 import { globalExceptionFilter } from './common/filters/global-exception.filter.js';
 import { httpLoggingInterceptor } from './common/interceptors/http-logging.interceptor.js';
 import { requestIdMiddleware } from './common/middleware/request-id.middleware.js';
@@ -25,35 +23,6 @@ app.get('/health', (_req, res) => {
 });
 
 app.use("/api/todo", taskRoutes);
-
-const swaggerSpec = swaggerJsdoc({
-  definition: {
-    openapi: '3.0.0',
-    info: { title: 'Todo Service API', version: '1.0.0' },
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT',
-        },
-      },
-    },
-    security: [
-      {
-        bearerAuth: [],
-      },
-    ],
-  },
-  apis: ['./src/routes/*.ts', './dist/routes/*.js'],
-});
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-app.get('/api/docs.json', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(swaggerSpec);
-});
 
 app.use((_request, _response, next) => {
   const notFoundError = Object.assign(new Error('Not Found'), { status: 404 });
